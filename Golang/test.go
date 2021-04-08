1c5970d5f8fec896a74c6473ab818cc3bc72ee5d
Kevin3599 <189818185@qq.com>
2021-04-08 19:07:59 +0800

    第二次补充提交

--- Golang/test.go
+++ Golang/test.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tabalt/ipquery"
+)
 
 func main() {
-    fmt.Println("hello world!")
+	varl1 := "testdata/test_10000.data"
+	err := ipquery.load
+	if err != nil {
+		fmt.Println(err)
+	}
+	ip := ""
+	varl1, err = ipquery.find(ip)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ip, string(varl1))
+	}
+}
+
 }
