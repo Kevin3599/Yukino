c069c25d6bc093e96f42cf770ee730bb5946ed9b
Kevin3599 <189818185@qq.com>
2021-04-08 09:17:20 +0800

    第一次提交

--- Golang/IP .go
+++ Golang/IP .go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"fmt"
-    "github.com/tabalt/ipquery"
 )
-    func main(){
-	  df :="testdata/test_10000.data"
-     err :=ipquery.load
-    if err!=nil {
+
+func main() {
+	df := "testdata/test_10000.data"
+	err := ipquery.load
+	if err != nil {
 		fmt.Println(err)
 	}
-    ip:=""
-    dt,err=ipquery.find(ip)
-    if err !=nil{
-	             fmt.println(error)
-	}else {
-	             fmt.println（ip，string(dt)）
-	} 
+	ip := ""
+	dt, err = ipquery.find(ip)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(ip, string(dt))
+	}
 }
-
