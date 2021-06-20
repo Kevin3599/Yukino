ee9e57b17dc0384499b980bb8eae255ed7ef99ce
Kevin3599 <69574926+Kevin3599@users.noreply.github.com>
2021-06-21 01:56:07 +0800

    第三次提交，补充完善功能

--- mp4.py
+++ mp4.py
@@ -2,12 +2,18 @@ class video(object):
      def __init__(self,path):
         self.path = path
        
-        def play(self):
-           from os import startfile
-           startfile(self.path)  
+     def play(self):
+        from os import startfile
+        startfile(self.path)  
 
 class Movie_mp4(video):
     type='MP4'
+
+class say_hello:
+     def __init__(self):
+            print("You were fooled!") 
+
+trick=say_hello()
 movie = Movie_mp4(r'')
-movie.py()
+movie.play()
 #A funny trick, isn’t it? 😀
